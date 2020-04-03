2976fd02d1e9bed891c435e527d3756b087c5cf7
Jake Lee <jhl2195@columbia.edu>
2020-04-03 18:51:16 -0400

    Delivery totals update Generalized specific designs to avoid confusion

--- components/totals.js
+++ components/totals.js
@@ -1,164 +1,230 @@
 export default [
   {
+    count: 200,
+    date: '04/03/20',
+    hospital: 'Queens',
+    type: 'Face Shields'
+  },
+  {
+    count: 100,
+    date: '04/03/20',
+    hospital: 'Queens',
+    type: 'Face Shields'
+  },
+  {
+    count: 200,
+    date: '04/03/20',
+    hospital: 'UWS',
+    type: 'Face Shields'
+  },
+  {
+    count: 90,
+    date: '04/02/20',
+    hospital: 'Brooklyn',
+    type: 'Face Shields'
+  },
+  {
+    count: 220,
+    date: '04/02/20',
+    hospital: 'Bronx',
+    type: 'Hand Sanitizer'
+  },
+  {
+    count: 56,
+    date: '04/02/20',
+    hospital: 'LES',
+    type: 'Sewn Masks'
+  },
+  {
+    count: 25,
+    date: '04/02/20',
+    hospital: 'Queens',
+    type: 'Face Shields'
+  },
+  {
+    count: 50,
+    date: '04/02/20',
+    hospital: 'UWS',
+    type: 'Face Shields'
+  },
+  {
+    count: 125,
+    date: '04/02/20',
+    hospital: 'UWS',
+    type: 'Face Shields'
+  },
+  {
+    count: 440,
+    date: '04/01/20',
+    hospital: 'UES',
+    type: 'N95 Masks'
+  },
+  {
+    count: 10,
+    date: '03/31/20',
+    hospital: 'Brooklyn',
+    type: 'Face Shields'
+  },
+  {
     count: 250,
     date: '03/31/20',
     hospital: 'Queens',
-    type: 'WISC Shields'
+    type: 'Face Shields'
   },
   {
     count: 100,
     date: '03/31/20',
     hospital: 'UWS',
-    type: 'WISC Shields'
+    type: 'Face Shields'
   },
   {
     count: 100,
     date: '03/31/20',
     hospital: 'Bronx',
-    type: 'WISC Shields'
+    type: 'Face Shields'
   },
   {
     count: 30,
     date: '03/30/20',
     hospital: 'UES',
-    type: 'WISC Shields'
+    type: 'Face Shields'
   },
   {
     count: 160,
     date: '03/30/20',
     hospital: 'Bronx',
-    type: 'WISC Shields'
+    type: 'Face Shields'
   },
   {
     count: 175,
     date: '03/30/20',
     hospital: 'LES',
-    type: 'WISC Shields'
+    type: 'Face Shields'
   },
   {
     count: 50,
     date: '03/30/20',
     hospital: 'Queens',
-    type: 'WISC Shields'
+    type: 'Face Shields'
   },
   {
     count: 30,
     date: '03/28/20',
     hospital: 'Queens',
-    type: 'WISC Shields'
+    type: 'Face Shields'
   },
   {
     count: 10,
     date: '03/28/20',
     hospital: 'Brooklyn',
-    type: 'WISC Shields'
+    type: 'Face Shields'
   },
   {
     count: 175,
     date: '03/27/20',
     hospital: 'Upper West Side',
-    type: '3DVerkstan Shields'
+    type: 'Face Shields'
   },
   {
     count: 300,
     date: '03/27/20',
     hospital: 'Upper West Side',
-    type: '3DVerkstan Shields'
+    type: 'Face Shields'
   },
   {
     count: 100,
     date: '03/27/20',
     hospital: 'Brooklyn',
-    type: 'WISC Shields'
+    type: 'Face Shields'
   },
   {
     count: 150,
     date: '03/26/20',
     hospital: 'Manhattan',
-    type: 'WISC Shields'
+    type: 'Face Shields'
   },
   {
     count: 30,
     date: '03/26/20',
     hospital: 'Brooklyn',
-    type: 'NYU Shields'
+    type: 'Face Shields'
   },
   {
     count: 100,
     date: '03/26/20',
     hospital: 'Manhattan',
-    type: '3DVerkstan Shields'
+    type: 'Face Shields'
   },
   {
     count: 115,
     date: '03/26/20',
     hospital: 'Manhattan',
-    type: '3DVerkstan Shields'
+    type: 'Face Shields'
   },
   {
     count: 50,
     date: '03/26/20',
     hospital: 'Manhattan',
-    type: 'WISC Shields'
+    type: 'Face Shields'
   },
   {
     count: 100,
     date: '03/25/20',
     hospital: 'Brooklyn',
-    type: 'WISC Shields'
+    type: 'Face Shields'
   },
   {
     count: 40,
     date: '03/25/20',
     hospital: 'Bronx',
-    type: 'NYU Shields'
+    type: 'Face Shields'
   },
   {
     count: 30,
     date: '03/25/20',
     hospital: 'Manhattan',
-    type: '3DVerkstan Shields'
+    type: 'Face Shields'
   },
   {
     count: 800,
     date: '03/25/20',
     hospital: 'Manhattan',
-    type: 'Shields'
+    type: 'Face Shields'
   },
   {
     count: 150,
     date: '03/24/20',
     hospital: 'Brooklyn',
-    type: 'WISC Shields'
+    type: 'Face Shields'
   },
   {
     count: 70,
     date: '03/24/20',
     hospital: 'Manhattan',
-    type: 'WISC Shields'
+    type: 'Face Shields'
   },
   {
     count: 75,
     date: '03/24/20',
     hospital: 'Queens',
-    type: 'WISC Shields'
+    type: 'Face Shields'
   },
   {
     count: 150,
     date: '03/23/20',
     hospital: 'Brooklyn',
-    type: 'WISC Shields'
+    type: 'Face Shields'
   },
   {
     count: 50,
     date: '03/22/20',
     hospital: 'Manhattan',
-    type: 'WISC Shields/1 Miniband'
+    type: 'Face Shields'
   },
   {
     count: 97,
     date: '03/22/20',
     hospital: 'Queens',
-    type: 'WISC Shields'
+    type: 'Face Shields'
   }
 ]
