0b8102ea1fdb602561e1264312698906766cc1b4
Robert O'Connor <rob@oconnor.ninja>
2020-04-04 20:12:22 -0400

    use 2020 versus 20

--- components/totals.jsx
+++ components/totals.jsx
@@ -1,271 +1,271 @@
 export default [
   {
     count: 100,
-    date: '04/04/20',
+    date: '04/04/2020',
     hospital: 'Bronx',
     type: 'Face Shields'
   },
   {
     count: 100,
-    date: '04/04/20',
+    date: '04/04/2020',
     hospital: 'Bronx',
     type: 'Face Shields'
   },
   {
     count: 100,
-    date: '04/04/20',
+    date: '04/04/2020',
     hospital: 'Bronx',
     type: 'Face Shields'
   },
   {
     count: 60,
-    date: '04/04/20',
+    date: '04/04/2020',
     hospital: 'Brooklyn',
     type: 'Face Shields'
   },
   {
-    count: 200,
-    date: '04/04/20',
+    count: 20200,
+    date: '04/04/2020',
     hospital: 'Queens',
     type: 'Face Shields'
   },
   {
     count: 100,
-    date: '04/04/20',
+    date: '04/04/2020',
     hospital: 'Queens',
     type: 'Face Shields'
   },
   {
     count: 100,
-    date: '04/03/20',
+    date: '04/03/2020',
     hospital: 'Queens',
     type: 'Sewn Masks'
   },
   {
-    count: 200,
-    date: '04/03/20',
+    count: 20200,
+    date: '04/03/2020',
     hospital: 'Queens',
     type: 'Face Shields'
   },
   {
     count: 100,
-    date: '04/03/20',
+    date: '04/03/2020',
     hospital: 'Queens',
     type: 'Face Shields'
   },
   {
-    count: 200,
-    date: '04/03/20',
+    count: 20200,
+    date: '04/03/2020',
     hospital: 'UWS',
     type: 'Face Shields'
   },
   {
     count: 90,
-    date: '04/02/20',
+    date: '04/02/2020',
     hospital: 'Brooklyn',
     type: 'Face Shields'
   },
   {
-    count: 220,
-    date: '04/02/20',
+    count: 22020,
+    date: '04/02/2020',
     hospital: 'Bronx',
     type: 'Hand Sanitizer'
   },
   {
     count: 56,
-    date: '04/02/20',
+    date: '04/02/2020',
     hospital: 'LES',
     type: 'Sewn Masks'
   },
   {
     count: 25,
-    date: '04/02/20',
+    date: '04/02/2020',
     hospital: 'Queens',
     type: 'Face Shields'
   },
   {
     count: 50,
-    date: '04/02/20',
+    date: '04/02/2020',
     hospital: 'UWS',
     type: 'Face Shields'
   },
   {
     count: 125,
-    date: '04/02/20',
+    date: '04/02/2020',
     hospital: 'UWS',
     type: 'Face Shields'
   },
   {
     count: 440,
-    date: '04/01/20',
+    date: '04/01/2020',
     hospital: 'UES',
     type: 'N95 Masks'
   },
   {
     count: 10,
-    date: '03/31/20',
+    date: '03/31/2020',
     hospital: 'Brooklyn',
     type: 'Face Shields'
   },
   {
     count: 250,
-    date: '03/31/20',
+    date: '03/31/2020',
     hospital: 'Queens',
     type: 'Face Shields'
   },
   {
     count: 100,
-    date: '03/31/20',
+    date: '03/31/2020',
     hospital: 'UWS',
     type: 'Face Shields'
   },
   {
     count: 100,
-    date: '03/31/20',
+    date: '03/31/2020',
     hospital: 'Bronx',
     type: 'Face Shields'
   },
   {
     count: 30,
-    date: '03/30/20',
+    date: '03/30/2020',
     hospital: 'UES',
     type: 'Face Shields'
   },
   {
     count: 160,
-    date: '03/30/20',
+    date: '03/30/2020',
     hospital: 'Bronx',
     type: 'Face Shields'
   },
   {
     count: 175,
-    date: '03/30/20',
+    date: '03/30/2020',
     hospital: 'LES',
     type: 'Face Shields'
   },
   {
     count: 50,
-    date: '03/30/20',
+    date: '03/30/2020',
     hospital: 'Queens',
     type: 'Face Shields'
   },
   {
     count: 30,
-    date: '03/28/20',
+    date: '03/28/2020',
     hospital: 'Queens',
     type: 'Face Shields'
   },
   {
     count: 10,
-    date: '03/28/20',
+    date: '03/28/2020',
     hospital: 'Brooklyn',
     type: 'Face Shields'
   },
   {
     count: 175,
-    date: '03/27/20',
+    date: '03/27/2020',
     hospital: 'Upper West Side',
     type: 'Face Shields'
   },
   {
     count: 300,
-    date: '03/27/20',
+    date: '03/27/2020',
     hospital: 'Upper West Side',
     type: 'Face Shields'
   },
   {
     count: 100,
-    date: '03/27/20',
+    date: '03/27/2020',
     hospital: 'Brooklyn',
     type: 'Face Shields'
   },
   {
     count: 150,
-    date: '03/26/20',
+    date: '03/26/2020',
     hospital: 'Manhattan',
     type: 'Face Shields'
   },
   {
     count: 30,
-    date: '03/26/20',
+    date: '03/26/2020',
     hospital: 'Brooklyn',
     type: 'Face Shields'
   },
   {
     count: 100,
-    date: '03/26/20',
+    date: '03/26/2020',
     hospital: 'Manhattan',
     type: 'Face Shields'
   },
   {
     count: 115,
-    date: '03/26/20',
+    date: '03/26/2020',
     hospital: 'Manhattan',
     type: 'Face Shields'
   },
   {
     count: 50,
-    date: '03/26/20',
+    date: '03/26/2020',
     hospital: 'Manhattan',
     type: 'Face Shields'
   },
   {
     count: 100,
-    date: '03/25/20',
+    date: '03/25/2020',
     hospital: 'Brooklyn',
     type: 'Face Shields'
   },
   {
     count: 40,
-    date: '03/25/20',
+    date: '03/25/2020',
     hospital: 'Bronx',
     type: 'Face Shields'
   },
   {
     count: 30,
-    date: '03/25/20',
+    date: '03/25/2020',
     hospital: 'Manhattan',
     type: 'Face Shields'
   },
   {
     count: 800,
-    date: '03/25/20',
+    date: '03/25/2020',
     hospital: 'Manhattan',
     type: 'Face Shields'
   },
   {
     count: 150,
-    date: '03/24/20',
+    date: '03/24/2020',
     hospital: 'Brooklyn',
     type: 'Face Shields'
   },
   {
     count: 70,
-    date: '03/24/20',
+    date: '03/24/2020',
     hospital: 'Manhattan',
     type: 'Face Shields'
   },
   {
     count: 75,
-    date: '03/24/20',
+    date: '03/24/2020',
     hospital: 'Queens',
     type: 'Face Shields'
   },
   {
     count: 150,
-    date: '03/23/20',
+    date: '03/23/2020',
     hospital: 'Brooklyn',
     type: 'Face Shields'
   },
   {
     count: 50,
-    date: '03/22/20',
+    date: '03/22/2020',
     hospital: 'Manhattan',
     type: 'Face Shields'
   },
   {
     count: 97,
-    date: '03/22/20',
+    date: '03/22/2020',
     hospital: 'Queens',
     type: 'Face Shields'
   }
