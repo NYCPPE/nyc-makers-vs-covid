a6435b317614b35913eeb7c284afb6d8cbf1159f
Callil Capuozzo <callil.capuozzo@gmail.com>
2020-04-06 10:34:36 -0400

    add banner

--- components/banner.jsx
+++ components/banner.jsx
@@ -4,17 +4,17 @@ export default function Banner() {
       <div className="max-w-screen-xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
         <div className="pr-16 sm:text-center sm:px-16">
           <p className="font-medium text-white">
-            <span className="hidden md:inline">COVID-19 ALERT:</span>
+            {/* <span className="hidden md:inline">COVID-19 ALERT:</span> */}
             <span className="">
-              Due to severe medical supply shortages hospitals are in dire need.
-              Can you help us?
+              Know how to sew? NYC Makes PPE is seeking volunteers to produce
+              100% cotton face masks.
             </span>
             <span className="block sm:m-2 sm:inline-block mt-2 md:m-0">
               <a
-                href="mailto:OpenFacePPEproject@gmail.com"
+                href="https://docs.google.com/document/d/1ZaKMhbZuQhKtEFLjgZBmOwH9hNSOdEz2jTONqMp7WbM/edit"
                 className="sm:p-2 text-white font-bold underline"
               >
-                Get in involved &rarr;
+                Find out more &rarr;
               </a>
             </span>
           </p>
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,6 +3,7 @@ import { MDXProvider } from '@mdx-js/react'
 import Header from '../components/header'
 import Nav from '../components/nav'
 import Footer from '../components/footer'
+import Banner from '../components/banner'
 import '../styles/index.css'
 
 const mdComponents = {}
@@ -15,6 +16,7 @@ export default ({ Component, pageProps }) => {
     >
       <Header />
       <Nav />
+      <Banner />
       <div className="markdown-body max-w-3xl pb-20 px-2 md:px-4 mx-auto">
         <Component {...pageProps} />
       </div>
