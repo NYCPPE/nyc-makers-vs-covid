c1f66f25eea14c7b961b814a3b439650ecc95062
Robert O'Connor <rob@oconnor.ninja>
2020-04-09 22:12:18 -0400

    Add media logos to press coverage component
    Closes #26

--- components/media.jsx
+++ components/media.jsx
@@ -3,6 +3,7 @@ const coverage = [
     href:
       'https://spectrum.ieee.org/tech-talk/biomedical/devices/lessons-learned-nyc-makers-producing-personal-protective-equipment-medics',
     organization: 'IEEE Spectrum',
+    logo: 'media_logos/ieee_spectrum.png',
     title:
       'Lessons Learned by NYC Makers Producing Personal Protective Equipment',
     date: '3/30/2020'
@@ -10,6 +11,7 @@ const coverage = [
   {
     href: 'https://www.youtube.com/watch?v=bAVtZ5mc4nM&t=61s',
     organization: 'Bloomberg TV',
+    logo: 'media_logos/bloomberg_tv.png',
     title: 'NYC Makes PPE Interview',
     date: '4/1/2020'
   },
@@ -17,6 +19,7 @@ const coverage = [
     href:
       'https://makezine.com/2020/04/03/makers-wanted-print-ppe-for-nyc-now/',
     organization: 'Make: Community',
+    logo: 'media_logos/make_community.png',
     title: 'Makers Wanted: Print PPE for NYC Now',
     date: '4/3/2020'
   }
@@ -28,12 +31,16 @@ export default () => (
       Press
     </div>
     <div className=" flex flex-col flex-nowrap justify rounded-lg">
-      {coverage.map(({ href, organization, title, date }, i) => (
+      {coverage.map(({ href, organization, logo, title, date }, i) => (
         <a
           href={href}
           key={i}
           className="block w-full rounded-lg over:bg-gray-100 text-base text-pink-600 overflow-hidden flex flex-col justify-between px-4 py-2"
         >
+          <img
+            className="object-left object-scale-down h-15 w-full"
+            src={logo}
+          />
           <div className="text-xl font-medium hover:underline">{title}</div>
           {organization} - {date}
         </a>
