b04b9c3605ae756c1251b2b42056b5aaefcddfbb
Robert O'Connor <rob@oconnor.ninja>
2020-04-26 00:40:28 -0400

    Remove dead code

--- components/products.jsx
+++ components/products.jsx
@@ -54,25 +54,6 @@ const product = [
     button: 'View Design'
   }
 ]
-const community = [
-  {
-    href: '/open-fold-face-shield.html',
-    label: 'Face Shield',
-    image: 'img/openfold.png',
-    title: 'Open Fold Face Shield',
-    text:
-      'These designs can be used to make ‘Face-shields’ using only thin plastic materials and some folding.'
-  },
-  {
-    href: '/fast-face-shield.html',
-    label: 'Fast Face Shield',
-    image: 'img/fastface.png',
-    title: 'Fast Face Shield',
-    text:
-      'The Fast Face Shield is a suggested design for healthcare workers with no other options.    ',
-    button: 'View Design'
-  }
-]
 
 export default () => (
   <>
@@ -101,32 +82,5 @@ export default () => (
         </a>
       ))}
     </div>
-    {/*
-    <div className="mt-12 mb-2 py-2 px-4 text-xl font-bold text-gray-900">
-      Community designs
-    </div>
-
-    <div className=" flex flex-row flex-wrap md:flex-no-wrap justify-between rounded-lg">
-      {community.map(({ href, label, title, text, button, image }, i) => (
-        <a
-          href={href}
-          key={i}
-          className="w-full md:w-1/2 rounded-lg hover:bg-gray-100 text-gray-900 overflow-hidden flex flex-col justify-between p-4"
-        >
-          <div className=" items-baseline">
-            <span className="rounded-lg">
-              <img
-                className="h-auto w-full mx-auto hidden md:block"
-                src={image}
-              />
-            </span>
-            <div className="pt-4 text-2xl leading-7 font-extrabold mb-2">
-              {title}
-            </div>
-            <div className="text-sm leading-5 mt-4">{text}</div>
-          </div>
-        </a>
-      ))}
-    </div> */}
   </>
 )
